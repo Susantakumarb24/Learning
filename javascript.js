4d0528913a7a1f961e6e30abd0887104e2a474b6
Susantakumarb24 <susantakumarb24@gmail.com>
2021-11-29 13:03:13 +0530

    ok

---
+++ javascript.js
@@ -0,0 +1,18 @@
+function on(){
+     document.getElementById('myImage').src='pic_bulbon.jpg';
+     }
+function off(){
+     document.getElementById('myImage').src='pic_bulboff.jpg';
+     }
+
+
+
+
+
+
+
+    
+  
+  
+    
+        
